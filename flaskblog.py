ed7939c17b9ae3224dc9ff752caa30ddbb725e1b
Michael-Schembri <michael_schembri@hotmail.co.uk>
2020-05-14 09:30:48 +0200

    Part 2: adapted Flaskblog to use templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author':'michael schembri',
+        'title':'cat ipsum',
+        'content':'is really funny',
+        'date_posted':'10-05-2020'
+    },
+    {
+        'author':'some random guy',
+        'title':'dog ipsum',
+        'content':'is not a thing funny',
+        'date_posted':'11-05-2020'
+    }
+    ]
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
